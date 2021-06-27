29d7f2173ec04f35b5df4ae2a33dbfbfb06cf607
Asif Estiak <me.asif.ae@gmail.com>
2021-06-27 20:49:31 +0600

    update expense item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
 
 function App() {
   return (
     <Fragment>
-      <div className="bg-dark text-white h100">
-        <h1>Let's get started!</h1>
+      <div className="container-fluid">
+        <div className="bg-dark text-white h100">
+          <h1>Let's get started!</h1>
+          <ExpenseItem></ExpenseItem>
+        </div>
       </div>
     </Fragment>
   );
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,9 +1,36 @@
 import React, { Fragment } from 'react';
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+  const expenseDate = new Date();
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  console.log(expenseDate)
   return (
     <Fragment>
-      <h1>Expense Item Component!</h1>
+      <div className="container">
+        <div className="p-3 border border-3 round-30 my-3">
+          <div className="row p-0 m-0">
+            <div className="col-3">
+              <div className="text-success">
+                <div className="">
+                  <h3>{expenseDate.toISOString()}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="">
+                <h3>{expenseTitle}</h3>
+              </div>
+            </div>
+            <div className="col-2">
+              <div className="border border-3 round-30 text-center bg-success">
+                <h3>${expenseAmount}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
