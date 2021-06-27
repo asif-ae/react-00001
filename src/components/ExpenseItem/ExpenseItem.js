3e69e64d11afef02c512a52a8d3bfc1336a59f6b
Asif Estiak <me.asif.ae@gmail.com>
2021-06-27 17:15:36 +0600

    added expense item component

---
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const ExpenseItem = () => {
+  return (
+    <Fragment>
+      <h1>Expense Item Component!</h1>
+    </Fragment>
+  );
+};
+
+export default ExpenseItem;
