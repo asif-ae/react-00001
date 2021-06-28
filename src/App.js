cccec3306e313a01da67dbcdd0f065001b6799d8
Asif Estiak <me.asif.ae@gmail.com>
2021-06-28 17:30:10 +0600

    updated expense item

--- src/App.js
+++ src/App.js
@@ -3,12 +3,40 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    { id: "e1", title: "Toilet Paper", amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: "e2", title: "New TV", amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: "e3", title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: "e4", title: "New Desk (Wooden)", amount: 294.67, date: new Date(2021, 2, 28) },
+  ]
+
+  console.log(expenses)
   return (
     <Fragment>
       <div className="container-fluid">
         <div className="bg-dark text-white h100">
           <h1>Let's get started!</h1>
-          <ExpenseItem></ExpenseItem>
+          <ExpenseItem
+            title={expenses[0].title}
+            amount={expenses[0].amount}
+            date={expenses[0].date}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={expenses[1].title}
+            amount={expenses[1].amount}
+            date={expenses[1].date}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={expenses[2].title}
+            amount={expenses[2].amount}
+            date={expenses[2].date}
+          ></ExpenseItem>
+          <ExpenseItem
+            title={expenses[3].title}
+            amount={expenses[3].amount}
+            date={expenses[3].date}
+          ></ExpenseItem>
         </div>
       </div>
     </Fragment>
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,11 +1,8 @@
 import React, { Fragment } from 'react';
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date();
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-  console.log(expenseDate)
+const ExpenseItem = ({ title, amount, date }) => {
+  
   return (
     <Fragment>
       <div className="container">
@@ -14,18 +11,18 @@ const ExpenseItem = () => {
             <div className="col-3">
               <div className="text-success">
                 <div className="">
-                  <h3>{expenseDate.toISOString()}</h3>
+                  <h3>{date.toISOString()}</h3>
                 </div>
               </div>
             </div>
             <div className="col-7">
               <div className="">
-                <h3>{expenseTitle}</h3>
+                <h3>{title}</h3>
               </div>
             </div>
             <div className="col-2">
               <div className="border border-3 round-30 text-center bg-success">
-                <h3>${expenseAmount}</h3>
+                <h3>${amount}</h3>
               </div>
             </div>
           </div>
